59fe043bef21d776af3728afca20e1f9717b50e8
ABShoto <animeshorts18@gmail.com>
2025-09-08 11:48:51 +0530

    Update statusbar.component.js

--- src/components/statusbar/statusbar.component.js
+++ src/components/statusbar/statusbar.component.js
@@ -117,19 +117,19 @@ class Statusbar extends Component {
       #tabs ul li[active]:nth-child(5) ~ li:last-child { margin: 0 0 0 140px; }
 
       #tabs ul li[active]:nth-child(1) ~ li:last-child {
-          --flavour: ${CONFIG.palette.green};
-      }
-
-      #tabs ul li[active]:nth-child(2) ~ li:last-child {
-          --flavour: ${CONFIG.palette.peach};
-      }
-
-      #tabs ul li[active]:nth-child(3) ~ li:last-child {
           --flavour: ${CONFIG.palette.red};
       }
 
-      #tabs ul li[active]:nth-child(4) ~ li:last-child {
+      #tabs ul li[active]:nth-child(2) ~ li:last-child {
           --flavour: ${CONFIG.palette.blue};
+      }
+
+      #tabs ul li[active]:nth-child(3) ~ li:last-child {
+          --flavour: ${CONFIG.palette.maroon};
+      }
+
+      #tabs ul li[active]:nth-child(4) ~ li:last-child {
+          --flavour: ${CONFIG.palette.peach};
       }
 
       #tabs ul li[active]:nth-child(5) ~ li:last-child {
